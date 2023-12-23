fcb69f29da3998f7e44d36b59248e3abc99ddf11
unknown <elahe.khabaz.bavani@gmail.com>
2023-12-23 23:17:46 +0330

    this is PDO/

--- index.php
+++ index.php
@@ -2,24 +2,25 @@
 
 <?php
 
-// $con= new mysqli("localhost","root","","blog_webprog");
-// $con->set_charset("utf8");
-// $query = "INSERT INTO `categoris` (`id`, `title`) VALUES (6, 'دسته 6')";
-// // $query = "DELETE FROM `categoris`";
-// // $query = "SELECT * FROM `categoris`";
+$con= new mysqli("localhost","root","","blog_webprog");
+$con->set_charset("utf8");
+$query = "INSERT INTO `categoris` (`id`, `title`) VALUES (6, 'دسته 6')";
+// $query = "DELETE FROM `categoris`";
+// $query = "SELECT * FROM `categoris`";
 
-// $result= $con->query($query);
+$result= $con->query($query);
 
-// if ($result === TRUE) {
-//     echo "Record inserted successfully.";
-// } else {
-//     echo "Error: " . $query . "<br>" . $con->error;
-// }
+if ($result === TRUE) {
+    echo "Record inserted successfully.";
+} else {
+    echo "Error: " . $query . "<br>" . $con->error;
+}
 
 ?>
 
 
 
+////////PDO/////////
 
 
 <?php
