c9ff02efe4bec6e325e8f7c72b450bc70d7982d1
unknown <elahe.khabaz.bavani@gmail.com>
2023-12-23 23:16:14 +0330

    this is PDO

--- index.php
+++ index.php
@@ -2,18 +2,38 @@
 
 <?php
 
-$con= new mysqli("localhost","root","","blog_webprog");
-$con->set_charset("utf8");
-$query = "INSERT INTO `categoris` (`id`, `title`) VALUES (6, 'دسته 6')";
-// $query = "DELETE FROM `categoris`";
-// $query = "SELECT * FROM `categoris`";
+// $con= new mysqli("localhost","root","","blog_webprog");
+// $con->set_charset("utf8");
+// $query = "INSERT INTO `categoris` (`id`, `title`) VALUES (6, 'دسته 6')";
+// // $query = "DELETE FROM `categoris`";
+// // $query = "SELECT * FROM `categoris`";
 
-$result= $con->query($query);
+// $result= $con->query($query);
 
-if ($result === TRUE) {
+// if ($result === TRUE) {
+//     echo "Record inserted successfully.";
+// } else {
+//     echo "Error: " . $query . "<br>" . $con->error;
+// }
+
+?>
+
+
+
+
+
+<?php
+
+$con = new PDO("mysql:host=localhost;dbname=blog_webprog;charset=utf8", "root", "");
+
+$query = "INSERT INTO categoris (id, title) VALUES (8, 'دسته 8')";
+
+$result = $con->query($query);
+
+if ($result === true) {
     echo "Record inserted successfully.";
 } else {
-    echo "Error: " . $query . "<br>" . $con->error;
+    echo "Error";
 }
 
 ?>
